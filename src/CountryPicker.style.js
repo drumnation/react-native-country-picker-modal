2300052a771830cc981a913aad085084ca34ea38
drumnation <davidmieloch@gmail.com>
2017-10-27 12:34:12 -0400

    style updates

--- src/CountryPicker.style.js
+++ src/CountryPicker.style.js
@@ -1,105 +1,102 @@
 // eslint-disable-next-line
-import { Platform, StyleSheet, PixelRatio } from "react-native";
-import { getWidthPercent, getHeightPercent } from "./ratio";
-import {
-  responsiveFontSize,
-  responsiveHeight,
-} from "react-native-responsive-dimensions";
+import { Platform, StyleSheet, PixelRatio } from 'react-native';
+import { getWidthPercent, getHeightPercent } from './ratio';
 
 export default StyleSheet.create({
   modalContainer: {
-    backgroundColor: "white",
-    flex: 1
+    backgroundColor: 'white',
+    flex: 1,
   },
   contentContainer: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "white"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
   },
   header: {
-    marginTop: Platform.OS === "ios" ? 16 : 0,
-    flexDirection: "row",
-    alignItems: "center"
+    marginTop: Platform.OS === 'ios' ? 16 : 0,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   input: {
     height: 48,
-    width: getWidthPercent(70)
+    width: getWidthPercent(70),
   },
   inputOnly: {
-    marginLeft: getWidthPercent(15)
+    marginLeft: getWidthPercent(15),
   },
   touchFlag: {
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   countryRow: {
-    flexDirection: "row"
+    flexDirection: 'row',
   },
   buttonText: {
-    fontFamily: Platform.OS === "ios" ? "Myriad Pro" : "myriadpro_regular",
-    color: "white",
-    textAlign: "center",
-    fontSize: responsiveFontSize(2),
-    marginTop: responsiveHeight(2.9)
+    alignSelf: 'center',
+    justifyContent: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'Myriad Pro' : 'myriadpro_regular',
+    color: 'white',
+    textAlign: 'center',
+    fontSize: getWidthPercent(4),
   },
   imgStyle: {
-    resizeMode: "contain",
+    resizeMode: 'contain',
     width: 25,
     height: 19,
     borderWidth: 1 / PixelRatio.get(),
-    borderColor: "#eee",
-    opacity: 0.8
+    borderColor: '#eee',
+    opacity: 0.8,
   },
   emojiFlag: {
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     fontSize: 30,
     width: 30,
     height: 30,
     borderWidth: 1 / PixelRatio.get(),
-    borderColor: "transparent",
-    backgroundColor: "transparent"
+    borderColor: 'transparent',
+    backgroundColor: 'transparent',
   },
   itemCountry: {
-    flexDirection: "row",
+    flexDirection: 'row',
     height: getHeightPercent(7),
-    justifyContent: "flex-start",
-    alignItems: "center"
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
   itemCountryFlag: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     height: getHeightPercent(7),
-    width: getWidthPercent(15)
+    width: getWidthPercent(15),
   },
   itemCountryName: {
-    justifyContent: "center",
+    justifyContent: 'center',
     width: getWidthPercent(70),
     borderBottomWidth: 2 / PixelRatio.get(),
-    borderBottomColor: "#ccc",
-    height: getHeightPercent(7)
+    borderBottomColor: '#ccc',
+    height: getHeightPercent(7),
   },
   countryName: {
-    fontSize: getHeightPercent(2.2)
+    fontSize: getHeightPercent(2.2),
   },
   scrollView: {
-    flex: 1
+    flex: 1,
   },
   letters: {
     marginRight: 10,
-    backgroundColor: "transparent",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   letter: {
     height: getHeightPercent(5.5),
     width: getWidthPercent(4),
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   letterText: {
-    textAlign: "center",
-    fontSize: getHeightPercent(3.1)
-  }
+    textAlign: 'center',
+    fontSize: getHeightPercent(3.1),
+  },
 });
